5b820972fc111fcb1159370fe646fc98a23b8f22
Shaharin G.R. <sganirashid23@gmail.com>
2024-11-27 08:30:03 +0000

    Updating Movement

--- Assets/Scripts/AnimationAndMovementControllers.cs
+++ Assets/Scripts/AnimationAndMovementControllers.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AnimationAndMovementControllers : MonoBehaviour
 {
     PlayerInput playerInput;
+    CharacterController CharacterController;
+    Animator animator;
 
     Vector2 currentMovementInput;
     Vector3 currentMovement;
@@ -13,20 +16,42 @@ public class AnimationAndMovementControllers : MonoBehaviour
     void Awake()
     {
         playerInput= new PlayerInput();
+        CharacterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
 
-        playerInput.CharacterControls.Move.started += context => {
+        playerInput.CharacterControls.Move.started += onMovementInput;
+        playerInput.CharacterControls.Move.canceled += onMovementInput;
+        playerInput.CharacterControls.Move.performed += onMovementInput;
+    }
+
+    void onMovementInput (InputAction.CallbackContext context)
+    {
             currentMovementInput = context.ReadValue<Vector2>();
             currentMovement.x = currentMovementInput.x;
             currentMovement.z = currentMovementInput.y;
             isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
-        };
     }
 
+    void handleAnimation()
+    {
+        bool isWalking = animator.GetBool("isWalking");
+        bool isRunning = animator.GetBool("isRunning");
+
+        if (isMovementPressed && !isWalking)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else if (!isMovementPressed && isWalking)
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        handleAnimation();
+        CharacterController.Move(currentMovement * Time.deltaTime);
     }
 
     void OnEnable()
